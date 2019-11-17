f6056a4912cc9a6a502d37b87054cfd36bb69a56
ColbyBurke <burkecl@g.cofc.edu>
2019-11-17 10:03:24 -0500

    whatever

--- main.js
+++ main.js
@@ -1,53 +1,186 @@
+//portraits, disney, whale, frisbee, ipad, computer, college
 
 const products = {
-    macbookair: {
-        title: "Macbook Air",
-        imageurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8q3B_-j5MEv-MI2DMb5fR-ZZGiXNOCeqwSG35GBrFFizO_I7f&s",
-        price: "$1200"
+    "nikoncoolpix": {
+        title: 'Nikon CoolPix Camera',
+        quality: 'disney',
+        imageurl: './images/nikoncoolpix.jpg',
+        price:'$175',
+        buylink: 'https://www.amazon.com/gp/product/B00BOZCUG2'
     },
-    macbookpro: {
-        title: "Macbook Pro",
-        imageurl: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1529520060550",
-        price: "$1200"
-    }
-}
+    "canonpowershot": {
+        title: 'Canon PowerShot',
+        quality: 'disney',
+        imageurl: './images/canonpowershot.jpg',
+        price:'$79',
+        buylink: 'https://www.amazon.com/gp/product/B00908BMVE'
+    },
+    "olympuscamera": {
+        title: 'Olympus Camera',
+        quality: 'disney',
+        imageurl: './images/olympuscamera.jpg',
+        price:'$185',
+        buylink: 'https://www.amazon.com/gp/product/B00803WNOK'
+    },
+    "griffin": {
+        title: 'Griffin iPad Cover',
+        quality: 'frisbee',
+        imageurl: './images/griffin.jpg',
+        price:'$60',
+        buylink: 'https://www.amazon.com/gp/product/B007XOQ3BQ'
+    },
+    "amzer": {
+        title: 'Amzer iPad Cover',
+        quality: 'frisbee',
+        imageurl: './images/amzer.jpg',
+        price:'$17',
+        buylink: 'https://www.amazon.com/gp/product/B00966J40Q'
+    },
+    "smartcase": {
+        title: 'Smart Case iPad Cover',
+        quality: 'frisbee',
+        imageurl: './images/smartcase.jpg',
+        price:'$14',
+        buylink: 'https://www.amazon.com/gp/product/B008B6UA8M'
+    },
+    "canoncamera": {
+        title: 'Canon Camera',
+        quality: 'video',
+        imageurl: './images/canoncamera.jpg',
+        price:'$598',
+        buylink: 'https://www.amazon.com/gp/product/B00894YWD0'
+    },
+    "nikoncamera": {
+        title: 'Nikon Camera',
+        quality: ['portraits','disney'],
+        imageurl: './images/nikoncamera.jpg',
+        price:'$226',
+        buylink: 'https://www.amazon.com/gp/product/B0073HSH8U'
+    },
+    "panasoniccamera": {
+        title: 'Panasonic Camera',
+        quality: ['whale','portraits','video'],
+        imageurl: './images/panasoniccamera.jpg',
+        price:'$124',
+        buylink: 'https://www.amazon.com/gp/product/B00728ZAEY'
+    },
+    "c50l": {
+        title: 'Canon 50mm Lens',
+        //quality: 'portraits',
+        imageurl: './images/c50l.jpg',
+        price:'$125',
+        buylink: 'https://www.amazon.com/gp/product/B00007E7JU'
+    },
+    "c85l": {
+        title: 'Canon 85mm Lens',
+        //quality: 'portraits',
+        imageurl: './images/c85l.jpg',
+        price:'$269',
+        buylink: 'https://www.amazon.com/gp/product/B00007GQLU'
+    },
+    "n50l": {
+        title: 'Nikon 50mm Lens',
+        //quality: 'portraits',
+        imageurl: './images/n50l.jpg',
+        price:'$131',
+        buylink: 'https://www.amazon.com/gp/product/B00005LEN4'
+    },
+    "sscb": {
+        title: 'Samsung Chromebook',
+        quality: 'college',
+        imageurl: './images/sscb.jpg',
+        price:'$84',
+        buylink: 'https://www.amazon.com/gp/product/B009LL9VDG'
+    },
+    "acer": {
+        title: 'Acer Chromebook',
+        quality: 'college',
+        imageurl: './images/acer.jpg',
+        price:'$103',
+        buylink: 'https://www.amazon.com/gp/product/B00GSEOV7U'
+    },
+    "mbp": {
+        title: 'Macbook Pro',
+        quality: 'college',
+        imageurl: './images/mbp.jpg',
+        price:'$424',
+ 
+ buylink: 'https://www.amazon.com/gp/product/B0074703CM'
+    },
+ }
 
 function displayInside() {
     document.getElementById('inside').innerHTML = ''
     let search = document.getElementById('searchbar').value.toLowerCase().split(' ').join('')
     let arr = []
+    let qualityArr = []
     let keys = Object.keys(products).map(x => x.split(' ').join(' ').toLocaleLowerCase())
+    console.log();
+
     console.log(keys);
     for (i = 0; i < keys.length; i++) {
         let regex = new RegExp(keys[i], 'gi')
         if (search.match(regex) !== null) arr.push(search.match(regex)[0])
+
     }
     console.log('arr ' + arr);
 
     document.getElementById('inside').innerHTML += '<ul id="list"></ul>'
-    for (i = 0; i < arr.length; i++) {
-        document.getElementById('list').innerHTML += "<li>" + `<img src=${products[arr[i]].imageurl}/>` + '<br/>' + products[arr[i]].title + '<br/>' +products[arr[i]].price +  "</li>"
-    }
 
-
-
-
-    /*
-    let keys = Object.keys(products)
-    let arr = []
-    for(i = 0; i < keys.length; i++){
-        let regex = new RegExp('/' + keys[i] + '/')
-        if(keys[i] === search.match(regex)[i]){
-            arr.push(keys[i])
+    if (search.includes('video') && !search.includes('portraits')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('video')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + '<br/>' + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+  "</li>"
+            }
         }
     }
-    
-    console.log(arr);
-    
-    
-    document.getElementById('inside').innerHTML = '<ul>' + arr.map(x => {
-        '<li>' + products[x].imageurl + '<br/>'
-    + products[x].title + '<br/>' + products[x].price + '</li>'
-    }) + '</ul>'
-    */
+
+    if (search.includes('video') && search.includes('portraits')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('video') && products[keys[i]].quality.includes('portraits')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
+    if (search.includes('portraits')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('portraits')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
+    if (search.includes('disney')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('disney')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
+    if (search.includes('whale')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('whale')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
+    if (search.includes('frisbee')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('frisbee')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
+    if (search.includes('prom')) {
+        for (i = 0; i < keys.length; i++) {
+            if (products[keys[i]].quality.includes('prom')) {
+                document.getElementById('list').innerHTML += "<li>" + `<img src=${products[keys[i]].imageurl} alt="nikon"/>` + '<br/>' + products[keys[i]].title + '<br/>' + products[keys[i]].price + `<button class="go" onclick="location.href='${products[keys[i]].buylink}'";>Buy Now</button>`+ "</li>"
+            }
+        }
+    }
+
 }
